2f09fb09af70045cfe2e8edb7512eec1aab582ec
Saleen Khadka <salinkhadka60@gmail.com>
2024-05-19 12:32:59 +0545

    same

--- SplashAndButtonNavigation/app/src/main/java/com/example/splashandbuttonnavigation/TabAndViewPager.kt
+++ SplashAndButtonNavigation/app/src/main/java/com/example/splashandbuttonnavigation/TabAndViewPager.kt
@@ -5,12 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.splashandbuttonnavigation.databinding.ActivityTabAndViewPagerBinding
 
 class TabAndViewPager : AppCompatActivity() {
+    lateinit var Binding: ActivityTabAndViewPagerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_tab_and_view_pager)
+        Binding=ActivityTabAndViewPagerBinding.inflate(layoutInflater)
+        setContentView(Binding.root)
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
