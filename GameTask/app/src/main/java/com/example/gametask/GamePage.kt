e4c177c358368ec2e2583adaf575804db201517c
Saleen Khadka <salinkhadka60@gmail.com>
2024-05-23 18:00:17 +0545

    added timer and used fuctions for readability

--- GameTask/app/src/main/java/com/example/gametask/GamePage.kt
+++ GameTask/app/src/main/java/com/example/gametask/GamePage.kt
@@ -2,249 +2,133 @@ package com.example.gametask
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.gametask.databinding.ActivityGamePageBinding
-
 import kotlin.random.Random
+
 class GamePage : AppCompatActivity() {
 
-    lateinit var GamePage:ActivityGamePageBinding
-    var Score:Int=0;
-    var life:Int=3;
+    lateinit var GamePage: ActivityGamePageBinding
+    var Score: Int = 0
+    var life: Int = 3
+    var number1 = Random.nextInt(0, 100)
+    var number2 = Random.nextInt(0, 100)
+    lateinit var timer: CountDownTimer
 
-
-
-    var number1= Random.nextInt(0,100)
-    var number2= Random.nextInt(0,100)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-
-        GamePage=ActivityGamePageBinding.inflate(layoutInflater)
+        GamePage = ActivityGamePageBinding.inflate(layoutInflater)
         setContentView(GamePage.root)
 
-        GamePage.textView.text="Score : "+Score
-        GamePage.textView2.text="Life : "+life
-        GamePage.textView3.text="Time : TODO"
-        GamePage.button2.isEnabled=false
-        var calc:String=intent.getStringExtra("operator").toString()
-
-        if (calc=="add") {
-
-       GamePage.textView4.text = "" + number1 + "   +  " + number2
-                GamePage.Check.setOnClickListener {
-                    GamePage.button2.isEnabled=true
-                    if (life > 0) {
-
-
-                        var input = GamePage.editTextText.text.toString();
-                        var checkInput = input.toIntOrNull() ?: 0;
-                        var ans: Int = (number1 + number2)
-
-                        if (checkInput == ans) {
-                            Score = Score + 10;
-                            GamePage.textView.text = "Score : " + Score
-                            GamePage.Check.isEnabled = false
-
-                            GamePage.button2.setOnClickListener {
-                                number1 = Random.nextInt(0, 100)
-                                number2 = Random.nextInt(0, 100)
-                                GamePage.textView4.text = "" + number1 + "   +  " + number2
-                                var input = GamePage.editTextText.text.toString();
-                                var checkInput = input.toIntOrNull() ?: 0;
-                                var ans: Int = (number1 + number2)
-
-
-                                GamePage.editTextText.setText("")
-                                GamePage.Check.isEnabled = true
-                                GamePage.button2.isEnabled=false
-
-
-                            }
-
-
-
-
-
-                        }
-                        else {
-
-                            life = life - 1;
-                            GamePage.textView2.text = "Life : " + life
-                            GamePage.button2.isEnabled=true
-                        }
-                    }
-                    else    {
-                        GamePage.textView4.text = "Game Over"
-                        GamePage.Check.isEnabled =false
-                        GamePage.button2.setOnClickListener {
-                            var intent=Intent(this@GamePage,GameOver::class.java)
-                            var message=Score.toString()
-                            intent.putExtra("Score",message)
-                            Log.d("message1",message)
-                            startActivity(intent)
-                            finish()
-                        }
-
-
-                    }
-                }
-
-
-
-        }
-
-        if (calc=="sub") {
-
-            GamePage.textView4.text = "" + number1 + "   -  " + number2
-            GamePage.Check.setOnClickListener {
-                GamePage.button2.isEnabled=true
-                if (life > 0) {
-
-
-                    var input = GamePage.editTextText.text.toString();
-                    var checkInput = input.toIntOrNull() ?: 0;
-                    var ans: Int = (number1-number2)
-
-                    if (checkInput == ans) {
-                        Score = Score + 10;
-                        GamePage.textView.text = "Score : " + Score
-                        GamePage.Check.isEnabled = false
-
-                        GamePage.button2.setOnClickListener {
-                            number1 = Random.nextInt(0, 100)
-                            number2 = Random.nextInt(0, 100)
-                            GamePage.textView4.text = "" + number1 + "   -  " + number2
-                            var input = GamePage.editTextText.text.toString();
-                            var checkInput = input.toIntOrNull() ?: 0;
-                            var ans: Int = (number1 - number2)
-
-
-                            GamePage.editTextText.setText("")
-                            GamePage.Check.isEnabled = true
-                            GamePage.button2.isEnabled=false
-
-
-                        }
-
-
-
-
-
-                    }
-                    else {
-
-                        life = life - 1;
-                        GamePage.textView2.text = "Life : " + life
-                        GamePage.button2.isEnabled=true
-                    }
-                }
-                else    {
-                    GamePage.textView4.text = "Game Over"
-                    GamePage.Check.isEnabled =false
-                    GamePage.button2.setOnClickListener {
-                        var intent=Intent(this@GamePage,GameOver::class.java)
-                        var message=Score.toString()
-                        intent.putExtra("Score",message)
-                        Log.d("message1",message)
-                        startActivity(intent)
-                        finish()
-                    }
-
-
-                }
-            }
-
-
-
-        }
-
-
-        if (calc=="mul") {
-
-            GamePage.textView4.text = "" + number1 + "   *  " + number2
-            GamePage.Check.setOnClickListener {
-                GamePage.button2.isEnabled=true
-                if (life > 0) {
-
-
-                    var input = GamePage.editTextText.text.toString();
-                    var checkInput = input.toIntOrNull() ?: 0;
-                    var ans: Int = (number1 * number2)
-
-                    if (checkInput == ans) {
-                        Score = Score + 10;
-                        GamePage.textView.text = "Score : " + Score
-                        GamePage.Check.isEnabled = false
-
-                        GamePage.button2.setOnClickListener {
-                            number1 = Random.nextInt(0, 100)
-                            number2 = Random.nextInt(0, 100)
-                            GamePage.textView4.text = "" + number1 + "   *  " + number2
-                            var input = GamePage.editTextText.text.toString();
-                            var checkInput = input.toIntOrNull() ?: 0;
-                            var ans: Int = (number1 * number2)
-
-
-                            GamePage.editTextText.setText("")
-                            GamePage.Check.isEnabled = true
-                            GamePage.button2.isEnabled=false
-
-
-                        }
-
-
-
-
-
-                    }
-                    else {
-
-                        life = life - 1;
-                        GamePage.textView2.text = "Life : " + life
-                        GamePage.button2.isEnabled=true
-                    }
-                }
-                else    {
-                    GamePage.textView4.text = "Game Over"
-                    GamePage.Check.isEnabled =false
-                    GamePage.button2.setOnClickListener {
-                        var intent=Intent(this@GamePage,GameOver::class.java)
-                        var message=Score.toString()
-                        intent.putExtra("Score",message)
-                        Log.d("message1",message)
-                        startActivity(intent)
-                        finish()
-                    }
-
-
-                }
-            }
-
-
-
-        }
-
-
-
-
-
-
-
-
-
-
-
+        GamePage.textView.text = "Score : $Score"
+        GamePage.textView2.text = "Life : $life"
+        GamePage.button2.isEnabled = false
+
+        val calc: String = intent.getStringExtra("operator").toString()
+        setQuestion(calc)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun setQuestion(calc: String) {
+        when (calc) {
+            "add" -> GamePage.textView4.text = "$number1 + $number2"
+            "sub" -> GamePage.textView4.text = "$number1 - $number2"
+            "mul" -> GamePage.textView4.text = "$number1 * $number2"
+        }
+        startTimer()
+        GamePage.Check.setOnClickListener {
+            checkAnswer(calc)
+        }
+    }
+
+    private fun checkAnswer(calc: String) {
+        GamePage.button2.isEnabled = true
+        if (life > 0) {
+            val input = GamePage.editTextText.text.toString()
+            val checkInput = input.toIntOrNull() ?: 0
+            val ans = when (calc) {
+                "add" -> number1 + number2
+                "sub" -> number1 - number2
+                "mul" -> number1 * number2
+                else -> 0
+            }
+
+            if (checkInput == ans) {
+                Score += 10
+                GamePage.textView.text = "Score : $Score"
+                GamePage.Check.isEnabled = false
+                GamePage.button2.setOnClickListener {
+                    nextQuestion(calc)
+                }
+            } else {
+                life--
+                GamePage.textView2.text = "Life : $life"
+                GamePage.button2.isEnabled = true
+                if (life <= 0) {
+                    gameOver()
+                }
+            }
+        } else {
+            gameOver()
+        }
+    }
+
+    private fun nextQuestion(calc: String) {
+        number1 = Random.nextInt(0, 100)
+        number2 = Random.nextInt(0, 100)
+        GamePage.editTextText.setText("")
+        GamePage.Check.isEnabled = true
+        GamePage.button2.isEnabled = false
+        setQuestion(calc)
+    }
+
+    private fun startTimer() {
+        timer = object : CountDownTimer(30000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                GamePage.textView3.text = "Time : ${millisUntilFinished / 1000}"
+            }
+
+            override fun onFinish() {
+                life--
+                GamePage.textView2.text = "Life : $life"
+                if (life > 0) {
+                    nextQuestion(intent.getStringExtra("operator").toString())
+                } else {
+                    gameOver()
+                }
+            }
+        }
+        timer.start()
+    }
+
+    private fun gameOver() {
+        GamePage.textView4.text = "Game Over"
+        GamePage.Check.isEnabled = false
+        timer.cancel()
+        GamePage.button2.setOnClickListener {
+            val intent = Intent(this@GamePage, GameOver::class.java)
+            val message = Score.toString()
+            intent.putExtra("Score", message)
+            Log.d("message1", message)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.cancel()
     }
 }
